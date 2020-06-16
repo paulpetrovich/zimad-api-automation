8db99c264881c84036cce2355c8178b20eadae09
ppetrovichidt <pavel.petrovich@idt.net>
2020-06-16 13:06:42 +0300

    Added initial maven dependencies Added the first empty test template

---
+++ src/test/java/Tasks/CreateNewTask.java
@@ -0,0 +1,31 @@
+package Tasks;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class CreateNewTask {
+
+    public class ApiHealthChecks {
+
+
+        @BeforeSuite
+        public void beforeSuite() {
+
+        }
+
+
+        @Test
+        public void FirstTest (){
+
+
+        }
+
+        @AfterSuite
+        public void afterSuite() {
+
+        }
+
+    }
+
+}
